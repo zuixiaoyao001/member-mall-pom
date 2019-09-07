f316f26831698d181fab620d3fcef6f01f086623
zuixiaoyao <1248375279@qq.com>
2019-09-07 13:08:46 +0800

    會員商城pom

---
+++ src/main/java/com/drawnblue/pom/PomApplication.java
@@ -0,0 +1,13 @@
+package com.drawnblue.pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomApplication.class, args);
+    }
+
+}
